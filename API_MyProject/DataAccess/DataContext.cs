1f64e125a6978a7a9cce1e0e1a8fcfd4d3a53e31
Kapoline <polina.chit@mail.ru>
2023-07-05 19:47:29 +0300

    add everything

---
+++ API_MyProject/DataAccess/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Entitis;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess;
+
+public class DataContext: DbContext
+{
+    public DbSet<Book> Books { get; set; } = null!;
+    public DbSet<Author> Authors { get; set; } = null!;
+    
+    public DataContext(DbContextOptions<DataContext> options): base(options){}
+}
